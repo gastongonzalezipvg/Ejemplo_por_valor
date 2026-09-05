86239da8ad4e9fc049ae251fc137a274299a34e5
Gaston <gaston.gonzalez@virginiogomez.cl>
2026-09-04 21:27:52 -0400

    Agregar archivos de proyecto.

---
+++ Ejemplo_por_valor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejemplo_por_valor;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            int c = Sumar(ref a, b);
+            Console.WriteLine($"La suma de {a} y {b} es {c}");
+        }
+
+        static int Sumar(ref int a, int b)
+        {
+            a = 2;
+        b = 4;
+            return a + b;
+        }
+    }
